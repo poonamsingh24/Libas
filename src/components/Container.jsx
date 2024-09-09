a0c6e9d3067ee98905c310a024915d90c6cb80f6
poonamsingh24 <poonams9994101@gmail.com>
2024-09-09 19:58:17 +0530

    for lg size container width increase

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -2,7 +2,9 @@ import React from "react";
 
 export default function Container({ children, className }) {
   return (
-    <div className={`max-w-[1400px] mx-auto px-4  ${className}`}>
+    <div
+      className={`max-w-[1400px] lg:max-w-[1800px] mx-auto px-4  ${className}`}
+    >
       {children}
     </div>
   );
